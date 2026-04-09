05f51799d57d9748f7867bb257fc5a0da857d6cc
SyddersLMAO <sydneyewens06@gmail.com>
2026-04-09 03:35:48 +0100

    updated api docs url

--- api/urls.py
+++ api/urls.py
@@ -5,11 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView
 
 app_name = 'api'
 
-router = DefaultRouter()
-
 urlpatterns = [
-    path('', include(router.urls)),
-
     path('items/', ContentListView.as_view()),
 
     path('item/<slug:slug>/', ContentDetailView.as_view()),
@@ -17,5 +13,5 @@ urlpatterns = [
     path('item/<slug:slug>/versions/', VersionListView.as_view()),
 
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('docs/', SpectacularRedocView.as_view(url_name='api:schema'), name='redoc'),
+    path('', SpectacularRedocView.as_view(url_name='api:schema'), name='redoc'),
 ]
